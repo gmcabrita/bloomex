e5aa9771d034ea6309c0f1fcb07f06b8da85ae76
Gonçalo Cabrita <_@gmcabrita.com>
2014-10-20 01:29:34 +0100

    Fix bitarray tests

--- test/Bitarray/bitarray_test.exs
+++ test/Bitarray/bitarray_test.exs
@@ -8,12 +8,12 @@ defmodule BitarrayTest do
 
   test "bitarray set and get" do
     ba = Bitarray.new(1009)
-    ba = Bitarray.set(1024, ba)
-    assert Bitarray.get(1024, ba) == true
+    ba = Bitarray.set(ba, 1024)
+    assert Bitarray.get(ba, 1024) == true
   end
 
   test "bitarray value not set" do
     ba = Bitarray.new(1)
-    assert Bitarray.get(5, ba) == false
+    assert Bitarray.get(ba, 5) == false
   end
 end
