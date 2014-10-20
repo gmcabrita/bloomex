d3afea476b77877f2dc0facb4028636343dd42d9
Gonçalo Cabrita <_@gmcabrita.com>
2014-10-20 01:26:25 +0100

    Add a few Bloomex tests

--- test/bloomex_test.exs
+++ test/bloomex_test.exs
@@ -1,4 +1,11 @@
 defmodule BloomexTest do
   use ExUnit.Case
 
+  test "bloomex add and check membership" do
+    bloom = Bloomex.new 1000, 0.5
+    assert Bloomex.member?(bloom, 100) == false
+
+    bloom = Bloomex.add bloom, 100
+    assert Bloomex.member?(bloom, 100) == true
+  end
 end
