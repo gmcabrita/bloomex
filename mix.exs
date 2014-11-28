a8d53c903464f3a2b25f1bff18c99c4e299c1c93
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-28 17:57:37 +0000

    Add dependencies to generate docs.

--- mix.exs
+++ mix.exs
@@ -7,11 +7,12 @@ defmodule Bloomex.Mixfile do
 
   def project do
     [app: :bloomex,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      description: @description,
      package: package,
      deps: deps,
+     aliases: [dialyze: "dialyze --unmatched-returns --error-handling --race-conditions --underspecs"],
      test_coverage: [tool: ExCoveralls]]
   end
 
@@ -20,17 +21,17 @@ defmodule Bloomex.Mixfile do
   end
 
   defp deps do
-    [
-      {:excoveralls, "~> 0.3", only: :dev}
+    [{:excoveralls, "~> 0.3", only: :dev},
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.6", only: :dev}
     ]
   end
 
   defp package do
-    [
-      files: ["lib", "mix.exs", "README.md", "LICENSE"],
-      contributors: ["Gonçalo Cabrita"],
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/gmcabrita/bloomex"}
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     contributors: ["Gonçalo Cabrita"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/gmcabrita/bloomex"}
     ]
   end
 end
