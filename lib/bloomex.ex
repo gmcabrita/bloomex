fde696c615946665f4f27ecfdafd5cab93373a11
Gonçalo Cabrita <_@gmcabrita.com>
2015-09-25 22:39:39 +0100

    Update paper link in the module docs as well

--- lib/bloomex.ex
+++ lib/bloomex.ex
@@ -1,6 +1,6 @@
 defmodule Bloomex do
   @moduledoc """
-  This module implements a [Scalable Bloom Filter](http://asc.di.fct.unl.pt/~nmp/pubs/ref--04.pdf).
+  This module implements a [Scalable Bloom Filter](http://haslab.uminho.pt/cbm/files/dbloom.pdf).
 
   ## Examples
 
