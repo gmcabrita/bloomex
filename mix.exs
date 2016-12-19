e3d9c4f8f60445b2e80d12f8779a2edec554a787
Gonçalo Cabrita <_@gmcabrita.com>
2016-12-19 01:43:07 +0000

    Update mix.exs and dependencies.

--- mix.exs
+++ mix.exs
@@ -4,42 +4,57 @@ defmodule Bloomex.Mixfile do
   @description """
   Bloomex is a pure Elixir implementation of Scalable Bloom Filters.
   """
+  @github "https://github.com/gmcabrita/bloomex"
 
   def project do
-    [app: :bloomex,
-     version: "1.0.1",
-     elixir: "~> 1.0",
-     description: @description,
-     package: package,
-     deps: deps,
-     aliases: [
-        dialyze: "dialyze \
-                    --unmatched-returns \
-                    --error-handling \
-                    --race-conditions \
-                    --underspecs"
+    [
+      app: :bloomex,
+      name: "Bloomex",
+      source_url: @github,
+      homepage_url: nil,
+      version: "1.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: @description,
+      package: package(),
+      deps: deps(),
+      dialyzer: [
+        flags: ["-Wunmatched_returns", "-Werror_handling", "-Wrace_conditions", "-Wunderspecs"]
       ],
-     test_coverage: [tool: ExCoveralls]]
+      test_coverage: [tool: ExCoveralls],
+      docs: docs()
+    ]
   end
 
   def application do
-    [applications: []]
+    []
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      logo: nil,
+      extras: ["README.md"]
+    ]
   end
 
   defp deps do
-    [{:excoveralls, "~> 0.3", only: :docs},
-     {:earmark, "~> 0.1", only: :docs},
-     {:ex_doc, "~> 0.10", only: :docs},
-     {:inch_ex, only: :docs},
-     {:dialyze, "~> 0.2.0", only: [:dev, :test]}
+    [
+      {:excoveralls, "~> 0.5", only: :docs, runtime: false},
+      {:ex_doc, "~> 0.14", only: :docs, runtime: false},
+      {:inch_ex, ">= 0.0.0", only: :docs, runtime: false},
+      {:dialyxir, "~> 0.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
-     maintainers: ["Gonçalo Cabrita"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/gmcabrita/bloomex"}
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Gonçalo Cabrita"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
     ]
   end
 end
