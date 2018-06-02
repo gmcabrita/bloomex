8aae7b2409aec33bec2267cc3d3847d653015560
Gonçalo Cabrita <_@gmcabrita.com>
2018-06-02 19:13:28 +0100

    bump dev version

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Bloomex.Mixfile do
       name: "Bloomex",
       source_url: @github,
       homepage_url: nil,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
